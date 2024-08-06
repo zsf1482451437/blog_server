a70bc5b18df62409e1033b95aefba11ecd640f65
ZSF <1482451437@qq.com>
2024-08-06 16:33:20 +0800

    fix: cors

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
+const cors = require("@koa/cors");
+
 const PageController = require("../server/controllers/PageController");
 
 const app = new Koa();
+
+// 允许特定域名跨域请求
+const corsOptions = {
+  origin: "https://zsf-blog.vercel.app",
+};
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser());
 app.use(PageController.routes());
