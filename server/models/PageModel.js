a20ee0cf7f4e829676215cdfa9bf9a471b010166
ZSF <1482451437@qq.com>
2024-08-06 16:22:40 +0800

    fix: db path

--- server/models/PageModel.js
+++ server/models/PageModel.js
@@ -1,8 +1,11 @@
 const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
+
+const dbPath = path.resolve(__dirname, "..", "database.db");
 
 class PageModel {
   constructor() {
-    this.db = new sqlite3.Database("database.db");
+    this.db = new sqlite3.Database(dbPath);
     this.initialize();
   }
 
